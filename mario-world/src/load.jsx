f3d0e18151e83438a87159eedb994e428d1530f6
keerthi9904 <pvslk9904@gmail.com>
2025-03-31 15:41:37 +0530

    added loading bar page

---
+++ mario-world/src/load.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./load.css";
+import MImage from "./assets/mario-m.png";
+
+const Load = () => {
+  const navigate = useNavigate();
+
+//   useEffect(() => {
+//     setTimeout(() => {
+//       navigate("/target"); // Replace with your actual target route
+//     }, 10000); // Adjust delay as needed
+//   }, [navigate]);
+
+  return (
+    <div className="loading-container">
+      <div className="loading-circle">
+        <img src={MImage} alt="M" className="m-image" />
+      </div>
+    </div>
+  );
+};
+
+export default Load;
